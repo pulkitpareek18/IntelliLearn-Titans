b65f5b97383dc8b1f5cde2f990cec38464682e57
Pulkit Pareek <84959590+pulkitpareek18@users.noreply.github.com>
2024-05-18 23:17:20 +0530

    added backend api url as env

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createContext } from "react";
 
-export const server = import.meta.env.VITE_AI_API_URL;
+export const server = import.meta.env.VITE_BACKEND_API_URL;
 
 export const Context = createContext({ isAuthenticated: false });
 
